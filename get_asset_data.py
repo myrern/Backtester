a36b999a807fc26fbaee4ff24885f35e352d6b1c
PederMyhrer <128213567+PederMyhrer@users.noreply.github.com>
2024-01-27 20:31:34 +0100

    gg

--- get_asset_data.py
+++ get_asset_data.py
@@ -43,7 +43,7 @@ def get_all_available_instruments(headers):
 
 def get_candles_to_pickle(instrument, type, headers):
     #granularities = ["S5", "S10", "S15", "S30", "M1", "M2", "M4", "M5", "M10", "M15", "M30", "H1", "H2", "H3", "H4", "H6", "H8", "H12", "D", "W", "M"]
-    granularities = ["S5"]
+    granularities = ["H1"]
     original_from_time = "2021-01-01T00:00:00Z"
     failed_gets = []
 
@@ -107,7 +107,10 @@ def get_candles_to_pickle(instrument, type, headers):
 #run functions
 def run_functions():
     instruments = get_all_available_instruments(headers)
-    for instrument in instruments:
-        get_candles_to_pickle(instrument["name"], instrument["type"], headers)
+    #for instrument in instruments:
+     #   get_candles_to_pickle(instrument["name"], instrument["type"], headers)
+
+    get_candles_to_pickle("EUR_USD", "CURRENCY", headers)
 
 run_functions()
+
